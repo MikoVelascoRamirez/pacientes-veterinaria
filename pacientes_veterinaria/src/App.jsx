e4d367a6db1aa9a891d67b8d52fbddf07bfcd9cd
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-07-22 16:54:10 -0500

    5. feat: Creating ListadoPacientes

--- pacientes_veterinaria/src/App.jsx
+++ pacientes_veterinaria/src/App.jsx
@@ -1,8 +1,14 @@
+import Formulario from '../components/Formulario';
 import Header from '../components/Header';
+import ListadoPacientes from '../components/ListadoPacientes';
 
 const App = () => {
   return (
-    <Header />
+    <div>
+      <Header />
+      <Formulario/>
+      <ListadoPacientes/>
+    </div>
   )
 }
 
