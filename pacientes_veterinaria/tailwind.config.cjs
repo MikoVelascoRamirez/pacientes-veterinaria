4d77a46114045ccfb04d10f810513b0c2681ac6e
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-07-29 15:34:11 -0500

    8. chore: creating news and editinglayouts screen's breakpoints

--- pacientes_veterinaria/tailwind.config.cjs
+++ pacientes_veterinaria/tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.jsx'],
+  content: ["./index.html", "./**/*.jsx"],
   theme: {
     extend: {},
+    screens: {
+      'xxs' : '160px',
+      'xs' : '320px',
+      'sm' : '640px',
+      'md' : '768px',
+      'lg' : '1024 px',
+      'xl' : '1280px',
+      '2xl' : '1536px'
+    }
   },
   plugins: [],
 }
