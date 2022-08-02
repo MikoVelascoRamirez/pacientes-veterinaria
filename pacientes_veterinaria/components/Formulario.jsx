18f9e5d9f5cba0fb0771e4e1f928c177425d7211
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-08-01 19:42:09 -0500

    14. feat: handling form validation

--- pacientes_veterinaria/components/Formulario.jsx
+++ pacientes_veterinaria/components/Formulario.jsx
@@ -1,5 +1,22 @@
+import {useState, useEffect} from 'react';
 
 const Formulario = () => {
+  const [paciente, setPaciente] = useState([]);
+  const [error, setError] = useState(false);
+  let pacientes = [];
+  let patient = {};
+
+  const handleSubmit = e => {
+    const form = document.querySelector('form');
+    e.preventDefault();
+
+    if([...form.querySelectorAll('.rounded-md')].map(a => a.value).includes('')){
+      setError(true);
+      return;
+    }
+    setError(false)
+  }
+
   return (
     <div className="sm:w-2/6 md:w-5/12 pb-10">
         <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
@@ -9,14 +26,23 @@ const Formulario = () => {
           <span className="text-indigo-600 font-bold"> Adminístralos</span>
         </p>
 
-        <form className="bg-white shadow-md rounded-lg py-8 px-5 mt-5">
+
+        <form 
+          onSubmit={handleSubmit}
+          className="bg-white shadow-md rounded-lg py-8 px-5 mt-5">
           <div className="mb-5">
+            
+            { error && <p>Algunos campos están vacíos</p> }
+            
             <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
             <input 
               type="text" 
               placeholder="Nombre de la mascota"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-              id="mascota"/>
+              id="mascota"
+              ////value={}={}
+              onChange={ e => patient.mascota = e.target.value}
+              />
           </div>
 
           <div className="mb-5">
@@ -25,6 +51,8 @@ const Formulario = () => {
               type="text" 
               placeholder="Nombre del propietario"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              ////value={}={paciente.propietario}              
+              onChange={ e => patient.propietario = e.target.value}
               id="propietario"/>
           </div>
 
@@ -34,6 +62,8 @@ const Formulario = () => {
               type="email" 
               placeholder="Email contacto propietario"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              //value={}={paciente.email}
+              onChange={ e => patient.email = e.target.value}
               id="email"/>
           </div>
 
@@ -43,6 +73,8 @@ const Formulario = () => {
               type="date" 
               placeholder="Nombre del propietario"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              //value={}={paciente.alta}
+              onChange={ e => patient.alta = e.target.value}
               id="alta"/>
           </div>
 
@@ -50,7 +82,9 @@ const Formulario = () => {
             <label htmlFor="sintomas" className="block text-gray-700 uppercase font-bold">Síntomas</label>
             <textarea
               placeholder="Describe los síntomas"
-              className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+              className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              //value={}={paciente.sintomas}
+              onChange={ e => patient.sintomas = e.target.value}
               id="sintomas"/>
           </div>
 
